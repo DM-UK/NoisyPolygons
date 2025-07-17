ba9dbf7d038fe1727af43853001149b31aeb6f32
DM-UK <dan.mills.uk@gmail.com>
2025-07-17 17:13:14 +0100

    initial

--- src/main/java/Blah.java
+++ src/main/java/Blah.java
@@ -4,6 +4,6 @@ import midpointdisplacement.MidpointDisplacedPath;
 public class Blah {
     public static void main(String[] args){
         MidpointDisplacedPath mdp;
-        JBasicForm form;
+        JBasicForm form = new JBasicForm();
     }
 }
